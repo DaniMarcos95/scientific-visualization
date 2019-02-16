6ed2c957c67e39395fcf0ef9d30cef5a70ed72e9
DaniMarcos95 <danimarcosmazon@gmail.com>
2019-02-16 13:29:51 +0100

    Initial commit

---
+++ Application.cpp
@@ -0,0 +1,133 @@
+#include <rfftw.h>              //the numerical simulation FFTW library
+#include <stdio.h>              //for printing the help text
+#include <math.h>               //for various math functions
+#include <GL/glut.h>            //the GLUT graphics library
+
+class Application{
+
+	friend class Visualization;
+	friend class Simulation;
+
+	Visualization vis;
+
+	public:
+		Application(Visualization)
+		void display(void);
+		void reshape(int, int);
+		void keyboard(unsigned char, int, int);
+		void drag(int, int);
+
+};
+
+Application::Application(Visualization visualization){
+	vis = visualization;
+}
+
+//display: Handle window redrawing events. Simply delegates to visualize().
+void Application::display(void)
+{
+	glClear(GL_COLOR_BUFFER_BIT | GL_DEPTH_BUFFER_BIT);
+	glMatrixMode(GL_MODELVIEW);
+	glLoadIdentity();
+	vis.visualize();
+	glFlush();
+	glutSwapBuffers();
+}
+
+//reshape: Handle window resizing (reshaping) events
+void Application::reshape(int w, int h)
+{
+ 	glViewport(0.0f, 0.0f, (GLfloat)w, (GLfloat)h);
+	glMatrixMode(GL_PROJECTION);
+	glLoadIdentity();
+	gluOrtho2D(0.0, (GLdouble)w, 0.0, (GLdouble)h);
+	winWidth = w; winHeight = h;
+}
+
+//keyboard: Handle key presses
+void Application::keyboard(unsigned char key, int x, int y)
+{
+	switch (key)
+	{
+	  case 't': dt -= 0.001; break;
+	  case 'T': dt += 0.001; break;
+	  case 'c': color_dir = 1 - color_dir; break;
+	  case 'S': vec_scale *= 1.2; break;
+	  case 's': vec_scale *= 0.8; break;
+	  case 'V': visc *= 5; break;
+	  case 'v': visc *= 0.2; break;
+	  case 'x': draw_smoke = 1 - draw_smoke;
+		    if (draw_smoke==0) draw_vecs = 1; break;
+	  case 'y': draw_vecs = 1 - draw_vecs;
+		    if (draw_vecs==0) draw_smoke = 1; break;
+	  case 'm': scalar_col++; if (scalar_col>COLOR_BANDS) scalar_col=COLOR_BLACKWHITE; break;
+	  case 'a': frozen = 1-frozen; break;
+	  case 'q': exit(0);
+	}
+}
+
+// drag: When the user drags with the mouse, add a force that corresponds to the direction of the mouse
+//       cursor movement. Also inject some new matter into the field at the mouse location.
+void Application::drag(int mx, int my)
+{
+	int xi,yi,X,Y; double  dx, dy, len;
+	static int lmx=0,lmy=0;				//remembers last mouse location
+
+	// Compute the array index that corresponds to the cursor location
+	xi = (int)clamp((double)(DIM + 1) * ((double)mx / (double)winWidth));
+	yi = (int)clamp((double)(DIM + 1) * ((double)(winHeight - my) / (double)winHeight));
+
+	X = xi; Y = yi;
+
+	if (X > (DIM - 1))  X = DIM - 1; if (Y > (DIM - 1))  Y = DIM - 1;
+	if (X < 0) X = 0; if (Y < 0) Y = 0;
+
+	// Add force at the cursor location
+	my = winHeight - my;
+	dx = mx - lmx; dy = my - lmy;
+	len = sqrt(dx * dx + dy * dy);
+	if (len != 0.0) {  dx *= 0.1 / len; dy *= 0.1 / len; }
+	fx[Y * DIM + X] += dx;
+	fy[Y * DIM + X] += dy;
+	rho[Y * DIM + X] = 10.0f;
+	lmx = mx; lmy = my;
+}
+
+
+
+
+
+int main(int argc, char **argv)
+{
+	printf("Fluid Flow Simulation and Visualization\n");
+	printf("=======================================\n");
+	printf("Click and drag the mouse to steer the flow!\n");
+	printf("T/t:   increase/decrease simulation timestep\n");
+	printf("S/s:   increase/decrease hedgehog scaling\n");
+	printf("c:     toggle direction coloring on/off\n");
+	printf("V/v:   increase decrease fluid viscosity\n");
+	printf("x:     toggle drawing matter on/off\n");
+	printf("y:     toggle drawing hedgehogs on/off\n");
+	printf("m:     toggle thru scalar coloring\n");
+	printf("a:     toggle the animation on/off\n");
+	printf("q:     quit\n\n");
+
+	Simulation sim;
+	Visualization vis;
+	Application app = Application(vis);
+
+
+	glutInit(&argc, argv);
+	glutInitDisplayMode(GLUT_RGB | GLUT_DOUBLE | GLUT_DEPTH);
+	glutInitWindowSize(500,500);
+	glutCreateWindow("Real-time smoke simulation and visualization");
+	glutDisplayFunc(app.display);
+	glutReshapeFunc(app.reshape);
+	glutIdleFunc(sim.do_one_simulation_step);
+	glutKeyboardFunc(app.keyboard);
+	glutMotionFunc(app.drag);
+
+	init_simulation(DIM);	//initialize the simulation data structures
+	glutMainLoop();			//calls do_one_simulation_step, keyboard, display, drag, reshape
+	return 0;
+}
---
+++ fftw-2.1.5/include/Visualization.h
@@ -0,0 +1,9 @@
+class Visualization{
+
+	public:
+		Visualization(int, int);
+		void rainbow(float,float*,float*,float*);
+		void set_colormap(float);
+		void direction_to_color(float, float, int);
+		void visualize(void);
+};
